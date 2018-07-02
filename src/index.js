aeff21af158b9a5215fd2a1a7b819034d78ee774
Fabien JUIF <fabien.juif@gmail.com>
2018-07-02 19:49:40 +0200

    :sparkles: first view(s)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-/* eslint-disable global-require */
+const path = require('path')
 const Koa = require('koa')
-const getSolde = require('./services/getSolde')
+const bodyParser = require('koa-body')
+const viewsMiddleware = require('koa-views')
+const api = require('./api')
+const views = require('./views')
 
 const PORT = process.env.PORT || 3000
 const LISTEN_HOST = process.env.LISTEN_HOST || '0.0.0.0'
 
 const app = new Koa()
 
-app.use(async (ctx) => {
-  try {
-    ctx.body = await getSolde()
-  } catch (e) {
-    console.trace(e)
+app.use(bodyParser({ multipart: true, urlencoded: true, strict: false }))
 
-    const { code, payload } = e
-    ctx.status = 500
-    ctx.body = { errors: [ { code, payload } ] }
-  }
-})
+app.use(viewsMiddleware(
+  path.resolve(__dirname, '..', 'views'),
+  {
+    extension: 'hbs',
+    map: {
+      hbs: 'handlebars',
+    },
+  },
+))
+
+app.use(api.routes()).use(api.allowedMethods())
+app.use(views.routes())
 
 const server = app.listen(PORT, LISTEN_HOST, () => {
   console.log(`Listening to ${LISTEN_HOST}:${PORT}`)
